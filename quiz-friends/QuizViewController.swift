22c7e155d95b021cb9f7376c32bd447c57a55a05
DuaLee <aidenlee01@gmail.com>
2022-04-24 12:09:55 -0400

    Skeleton code for changing gameMode and multi-peer selection

---
+++ quiz-friends/QuizViewController.swift
@@ -0,0 +1,32 @@
+//
+//  QuizViewController.swift
+//  quiz-friends
+//
+//  Created by Cony Lee on 4/24/22.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    
+    var gameMode: Int = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print(gameMode)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- quiz-friends/ViewController.swift
+++ quiz-friends/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var singleButton: UIButton!
+    @IBOutlet weak var multiButton: UIButton!
+    
+    @IBOutlet weak var playButton: UIButton!
+    
+    var gameMode: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        switch sender.tag {
+        case 1:
+            singleButton.isSelected = true
+            multiButton.isSelected = false
+            
+            gameMode = 1
+            
+        case 2:
+            singleButton.isSelected = false
+            multiButton.isSelected = true
+            
+            gameMode = 2
+            
+        default:
+            gameMode = 0
+        }
+        
+        if gameMode > 0 {
+            playButton.isEnabled = true
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            switch identifier {
+            case "quizSegue":
+                let controller = segue.destination as! QuizViewController
+                
+                controller.gameMode = self.gameMode
+            default:
+                break
+            }
+        }
+    }
+    
+    
 }
-
