08a184e4f9cd37531312d4f1bd006eafe7c4d80a
DuaLee <aidenlee01@gmail.com>
2022-04-24 16:18:54 -0400

    Status messages, fix UI constraints, limit connection

--- quiz-friends/ViewController.swift
+++ quiz-friends/ViewController.swift
@@ -12,8 +12,9 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
 
     @IBOutlet weak var singleButton: UIButton!
     @IBOutlet weak var visibilityButton: UIButton!
-    
     @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var statusLabel: UILabel!
     
     var gameMode: Int = 0
     var isHost: Bool = false
@@ -35,18 +36,19 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
         
         visibilityButton.setTitle(" Disable Device Visibility", for: .selected)
         visibilityButton.setTitle(" Enable Device Visibility", for: .normal)
-        visibilityButton.titleLabel?.font = .systemFont(ofSize: 24)
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         switch sender.tag {
         case 1:
+            statusLabel.text = "Single player selected."
+            
             singleButton.isSelected = true
             visibilityButton.isSelected = false
             playButton.isEnabled = true
             
-            stopClient()
             session.disconnect()
+            stopClient()
             
             gameMode = 1
             
@@ -54,10 +56,12 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
             singleButton.isSelected = false
             
             if visibilityButton.isSelected {
+                statusLabel.text = "Device invisible to nearby."
                 visibilityButton.isSelected = false
                 
                 stopClient()
             } else {
+                statusLabel.text = "Device visible to nearby."
                 visibilityButton.isSelected = true
                 
                 startClient()
@@ -70,10 +74,11 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
         }
     }
     
-    @IBAction func connectButtonPressed(_ sender: UIButton) {
+    @IBAction func hostButtonPressed(_ sender: UIButton) {
         singleButton.isSelected = false
         visibilityButton.isSelected = false
         
+        session.disconnect()
         stopClient()
         startServer()
     }
@@ -88,21 +93,34 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
     
     func startServer() {
         let browser = MCBrowserViewController(serviceType: "game", session: session)
+        browser.maximumNumberOfPeers = 4
         browser.delegate = self
         present(browser, animated: true)
     }
     
     func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
-        DispatchQueue.main.async(execute: {
+        DispatchQueue.main.async(execute: { [self] in
             switch state {
             case MCSessionState.connected:
                 print("Connected: \(peerID.displayName)")
                 
+                if !isHost {
+                    statusLabel.text = "Connected to host: \(peerID.displayName)."
+                }
+                
             case MCSessionState.connecting:
                 print("Connecting: \(peerID.displayName)")
                 
+                if !isHost {
+                    statusLabel.text = "Connecting to host: \(peerID.displayName)"
+                }
+                
             case MCSessionState.notConnected:
                 print("Not Connected: \(peerID.displayName)")
+                
+                if !isHost {
+                    statusLabel.text = "Disconnected from host: \(peerID.displayName)"
+                }
                 
             default:
                 print("Error")
@@ -129,6 +147,8 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
     func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
         // Called when the browser view controller is dismissed
         isHost = true
+        statusLabel.text = "You are the host."
+        playButton.isEnabled = true
         
         dismiss(animated: true, completion: nil)
     }
@@ -136,6 +156,8 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
     func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
         // Called when the browser view controller is cancelled
         isHost = false
+        statusLabel.text = "You are no longer the host."
+        playButton.isEnabled = false
         
         session.disconnect()
         
@@ -146,6 +168,8 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
         let acceptAction = UIAlertAction(title: "Accept",
                                          style: .default) { [self] action in
             invitationHandler(true, session)
+            visibilityButton.isSelected = false
+            playButton.isEnabled = false
             stopClient()
         }
         let declineAction = UIAlertAction(title: "Decline",
@@ -163,12 +187,6 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
     override func viewWillAppear(_ animated: Bool) {
         singleButton.isSelected = false
         visibilityButton.isSelected = false
-        
-        if isHost {
-            playButton.isEnabled = true
-        } else {
-            playButton.isEnabled = false
-        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
