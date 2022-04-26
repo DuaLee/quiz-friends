5ee263666cbde74054204989d81283adf2d4ed9d
lannahdavis <80347204+lannahdavis@users.noreply.github.com>
2022-04-26 19:03:05 -0400

    Update QuizViewController.swift
    Started reading from the URL QuizVC

--- quiz-friends/QuizViewController.swift
+++ quiz-friends/QuizViewController.swift
@@ -2,7 +2,7 @@
 //  QuizViewController.swift
 //  quiz-friends
 //
-//  Created by Cony Lee on 4/24/22.
+//  Created by Lannah Bell on 4/25/22.
 //
 
 import UIKit
@@ -10,16 +10,73 @@ import UIKit
 class QuizViewController: UIViewController {
     
     var gameMode: Int = 0
+    var numQuestions = [Int]()
+    @IBOutlet weak var questionLabel: UILabel!
+    @Published var quizData = [QuizData]()
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       
+       // questionLabel.text = "Question 0/\(numQuestions[0])"
+        
+        let urlString = "http://www.people.vcu.edu/~ebulut/jsonFiles/quiz1.json"
 
+        self.loadJson(fromURLString: urlString) { (result) in
+            switch result {
+            case .success(let data):
+                self.parse(jsonData: data)
+            case .failure(let error):
+                print(error)
+            }
+        }
+       
         print(gameMode)
         // Do any additional setup after loading the view.
     }
     
+    private func parse(jsonData: Data) {
+        do {
+            let decodedData = try JSONDecoder().decode(QuizData.self, from: jsonData)
+            
+            print("Number of questions: ", decodedData.numberOfQuestions)
+            numQuestions.append(decodedData.numberOfQuestions)
+           // print("Questions: ", decodedData.questions)
+            //print("Question num: ", decodedData.questionNumber)
+            print("Topic: ", decodedData.topic)
+            print(numQuestions)
+           // numQuestions = decodedData.numberOfQuestions
+            
+            //print("Description: ", decodedData.description)
+            print("===================================")
+        } catch {
+            print("decode error")
+        }
+    }
     
     
+    private func loadJson(fromURLString urlString: String,
+                          completion: @escaping (Result<Data, Error>) -> Void) {
+        if let url = URL(string: urlString) {
+            let urlSession = URLSession(configuration: .default).dataTask(with: url) { (data, response, error) in
+                if let error = error {
+                    completion(.failure(error))
+                }
+                
+                if let data = data {
+                    DispatchQueue.main.async {
+                        self.questionLabel.text = "Question 0/\(self.numQuestions[0])"
+                    } 
+                    completion(.success(data))
+                }
+            }
+            
+            urlSession.resume()
+        }
+    }
+    
+   
     /*
     // MARK: - Navigation
 
