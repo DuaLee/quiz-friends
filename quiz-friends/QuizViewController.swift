89ba4bddeea3904d1ade42400ed8fe66f644bf45
DuaLee <aidenlee01@gmail.com>
2022-04-30 16:29:46 -0400

    Finish merge of json parser

--- quiz-friends/QuizViewController.swift
+++ quiz-friends/QuizViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Cony Lee on 4/24/22.
 //
-
 import UIKit
 
 class QuizViewController: UIViewController {
@@ -20,13 +19,16 @@ class QuizViewController: UIViewController {
     @IBOutlet weak var buttonC: UIButton!
     @IBOutlet weak var buttonD: UIButton!
     
-    
+    @IBOutlet weak var questionLabel: UILabel!
     
     var gameMode: Int = 0
-
+    
+    var numQuestions = [Int]()
+    @Published var quizData = [QuizData]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+      
         playerIcons = [player1, player2, player3, player4]
         
         for playerIcon in playerIcons {
@@ -34,21 +36,60 @@ class QuizViewController: UIViewController {
             playerIcon.isSelected = false
             playerIcon.setTitle("", for: .normal)
         }
+      
+        // questionLabel.text = "Question 0/\(numQuestions[0])"
+        
+        let urlString = "http://www.people.vcu.edu/~ebulut/jsonFiles/quiz1.json"
 
+        self.loadJson(fromURLString: urlString) { (result) in
+            switch result {
+            case .success(let data):
+                self.parse(jsonData: data)
+            case .failure(let error):
+                print(error)
+            }
+        }
+       
         print(gameMode)
-        // Do any additional setup after loading the view.
+    }
+    
+    private func parse(jsonData: Data) {
+        do {
+            let decodedData = try JSONDecoder().decode(QuizData.self, from: jsonData)
+            
+            print("Number of questions: ", decodedData.numberOfQuestions)
+            numQuestions.append(decodedData.numberOfQuestions)
+           // print("Questions: ", decodedData.questions)
+            //print("Question num: ", decodedData.questionNumber)
+            print("Topic: ", decodedData.topic)
+            print(numQuestions)
+           // numQuestions = decodedData.numberOfQuestions
+            
+            //print("Description: ", decodedData.description)
+            print("===================================")
+        } catch {
+            print("decode error")
+        }
     }
     
     
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func loadJson(fromURLString urlString: String,
+                          completion: @escaping (Result<Data, Error>) -> Void) {
+        if let url = URL(string: urlString) {
+            let urlSession = URLSession(configuration: .default).dataTask(with: url) { (data, response, error) in
+                if let error = error {
+                    completion(.failure(error))
+                }
+                
+                if let data = data {
+                    DispatchQueue.main.async {
+                        self.questionLabel.text = "Question 0/\(self.numQuestions[0])"
+                    }
+                    completion(.success(data))
+                }
+            }
+            
+            urlSession.resume()
+        }
     }
-    */
-
 }
